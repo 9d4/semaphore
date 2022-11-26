d9231f9dc8d6608804e24a567a8d8a52694ee4f8
Dimanda <traperwaze@gmail.com>
2022-11-27 05:20:46 +0700

    init viper and config defaults

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/9d4/semaphore/server"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = cobra.Command{
@@ -14,4 +16,28 @@ var rootCmd = cobra.Command{
 	Run: boot(func(cmd *cobra.Command, args []string, passData *bootData) {
 		log.Fatal(server.Start())
 	}),
+}
+
+var serverFlags = flag.NewFlagSet(rootCmd.Name(), flag.ContinueOnError)
+
+func init() {
+	initFlags()
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().AddFlagSet(serverFlags)
+	viper.BindPFlags(serverFlags)
+}
+
+func initConfig() {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+}
+
+func initFlags() {
+	serverFlags.StringP("addr", "a", "0.0.0.0:3500", "Address to listen on")
+	serverFlags.String("dbhost", "127.0.0.1", "Database host")
+	serverFlags.String("dbport", "5432", "Database port")
+	serverFlags.String("dbname", "semaphore", "Database name")
 }
