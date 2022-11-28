7f7841121bdf27a560a2e96cf9b6b5762aec41eb
Dimanda <traperwaze@gmail.com>
2022-11-28 18:30:57 +0700

    refine dashboard, add greeting

---
+++ views/src/components/GreetingTron.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-3xl">Hey, {{ name }}👋🏼</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+};
+</script>
--- views/src/views/DashView.vue
+++ views/src/views/DashView.vue
@@ -1,7 +1,87 @@
 <template>
-  <div>
-    <p>Login Success</p>
+  <div class="bg-zinc-900">
+    <div class="container md:w-7/12 mx-auto px-4 pt-20">
+      <div class="tabs text-slate-50 w-full">
+        <RouterLink
+          to="/"
+          class="tab tab-lifted"
+          :class="{ 'tab-active': active == '' || active == 'home' }"
+          >Home
+        </RouterLink>
+        <RouterLink
+          to="/profile"
+          class="tab tab-lifted"
+          :class="{ 'tab-active': active == 'profile' }"
+          >Profile
+        </RouterLink>
+        <span class="tab tab-lifted flex-auto pointer-events-none"></span>
+      </div>
+    </div>
+  </div>
+  <div class="container md:w-7/12 mx-auto px-4 mt-5">
+    <component :is="view" v-bind="currentViewProps" />
   </div>
 </template>
 
-<script></script>
+<script>
+document.getElementsByTagName("html")[0].setAttribute("data-theme", "dracula");
+import GreetingTron from "../components/GreetingTron.vue";
+import { useAuthStore } from "../stores/auth";
+
+export default {
+  setup() {
+    const authStore = useAuthStore();
+    return { authStore };
+  },
+  components: { GreetingTron },
+  data() {
+    return {
+      active: "",
+      view: null,
+      currentViewProps: {},
+    };
+  },
+  created() {
+    this.$watch(
+      () => this.$route.params,
+      () => {
+        this.update();
+      }
+    );
+  },
+  mounted() {
+    this.update();
+  },
+  methods: {
+    update() {
+      this.handleTabs();
+      this.updateView();
+    },
+    handleTabs() {
+      const menu = this.$route.params.menu;
+      switch (menu) {
+        case "":
+        case "profile":
+          this.active = menu;
+          break;
+        default:
+          this.$router.push({ path: "/" });
+      }
+
+      this.updateView();
+    },
+    updateView() {
+      switch (this.active) {
+        case "":
+          this.view = "GreetingTron";
+          this.currentViewProps = {
+            name: this.authStore.jwt.User.FirstName,
+          };
+          break;
+        default:
+          this.view = null;
+      }
+    },
+  },
+};
+</script>
