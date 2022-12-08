62468e8067910c48be39bd2c3894cae89d28bb1e
Dimanda <traperwaze@gmail.com>
2022-12-08 21:01:15 +0700

    add utility to convert []byte to string and vice versa

---
+++ util/convert.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func StringToBytes(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
+}
