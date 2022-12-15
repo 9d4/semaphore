910c011295623df3451e9419cbd17f2a3ffa08ac
Dimanda <traperwaze@gmail.com>
2022-12-15 07:32:14 +0700

    refine oauth app model

--- oauth/app.go
+++ oauth/app.go
@@ -2,10 +2,15 @@ package oauth
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type App struct {
-	gorm.Model
-	Name     string `json:"name"`
-	ClientID string `json:"client_id" gorm:"unique,index"`
+	ID           uint           `gorm:"primarykey" json:"id"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
+	Name         string         `json:"name"`
+	ClientID     string         `json:"client_id" gorm:"uniqueIndex"`
+	ClientSecret string         `json:"-"`
 }
