809125a87d9e4dc8d25373ae390297efbd63b757
Dimanda <traperwaze@gmail.com>
2022-12-25 19:05:17 +0700

    update oauth add cmd

--- cmd/oauth.go
+++ cmd/oauth.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
-	"github.com/9d4/semaphore/oauth"
+	"github.com/9d4/semaphore/oauth2/models"
+	"github.com/9d4/semaphore/oauth2/store"
 	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
 	"os"
@@ -23,19 +25,32 @@ var oAuthCmd = &cobra.Command{
 }
 
 var oAuthAddCmd = &cobra.Command{
-	Use:   "add [app-name]",
+	Use:   "add [app-id] [app-domain] [app-secret]",
 	Short: "Add new client app",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(3),
 	Run: boot(func(cmd *cobra.Command, args []string, passData *bootData) {
-		oauthAppStore := oauth.NewStore(passData.db, passData.rdb)
-		app, err := oauthAppStore.CreateAuto(args[0])
+		clientStore := store.NewClientStoreRedis(passData.rdb)
+		err := clientStore.Set(args[0], &models.Client{
+			ID:     args[0],
+			Secret: args[2],
+			Domain: args[1],
+		})
 		if err != nil {
 			jww.FATAL.Fatal(err)
+			return
 		}
+
+		cli, err := clientStore.GetByID(context.Background(), args[0])
+		if err != nil {
+			jww.FATAL.Fatal(err)
+			return
+		}
+
 		tw := tabwriter.NewWriter(os.Stdout, 4, 4, 2, ' ', 0)
 		fmt.Println("Created!")
-		fmt.Fprintf(tw, "Name\t :%s\n", app.Name)
-		fmt.Fprintf(tw, "ClientID\t :%s\n", app.ClientID)
+		fmt.Fprintf(tw, "ClientID\t :%s\n", cli.GetID())
+		fmt.Fprintf(tw, "Secret\t :%s\n", cli.GetSecret())
+		fmt.Fprintf(tw, "Domain\t :%s\n", cli.GetDomain())
 		tw.Flush()
 	}),
 }
